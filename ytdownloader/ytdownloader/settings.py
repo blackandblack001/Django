6ef3c55b0afe0674cdc29261f4e348d2efaa8ea7
blackandblack001 <sksenthil838@gmail.com>
2022-07-29 18:16:24 +0530

    update

--- ytdownloader/ytdownloader/settings.py
+++ ytdownloader/ytdownloader/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,9 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_files")]
+
+django_on_heroku.settings(locals())
